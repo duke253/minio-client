72b40895a579b1ee5b4c2c2bd3cd8b7092d7e8d1
Grigoriy I <g.duke253@gmail.com>
2021-02-02 15:44:47 +0300

    fix issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 # Read environment variables or set default values
 echo -ne "Read environment variables..."
-MINIO_URL=${MINIO_URL:http://130.193.58.121:9000}
-MINIO_BUCKET=${MINIO_BUCKET:mm-data}
+MINIO_URL=${MINIO_URL:-http://130.193.58.121:9000}
+MINIO_BUCKET=${MINIO_BUCKET:-mm-data}
 MINIO_KEY=${MINIO_KEY:-AKIAIOSFODNN7EXAMPLE}
 MINIO_SECRET=${MINIO_SECRET:-AKIAIOSFODNN7EXAMPLE}
 
-echo MINIO_URL
-echo MINIO_BUCKET
-echo MINIO_KEY
-echo MINIO_SECRET
+echo $MINIO_URL
+echo $MINIO_BUCKET
+echo $MINIO_KEY
+echo $MINIO_SECRET
 
 exec /home/minio-client/mc config host add minio $MINIO_URL $MINIO_KEY $MINIO_SECRET
 exec /home/minio-client/mc ls minio/mm-data
